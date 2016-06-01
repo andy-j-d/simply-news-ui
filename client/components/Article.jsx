1764c5022736565ce0c5998022556d069f3ad513
Andrew Dushane <andrewdushane@gmail.com>
2016-05-31 23:04:10 -0400

    Scrub article title and description Remove tags and character codes

--- client/components/Article.jsx
+++ client/components/Article.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 
-import { Row, Col, Jumbotron } from 'react-bootstrap';
+const scrubText = (text) => text && text.replace(/(<\/?[^>]+(>|$))|(&(.*?);)/g, "");
 
 const Article = ({link, description, title, display_date}) => {
-
-  description = description && description.replace(/<\/?[^>]+(>|$)/g, "");
 
   return(
     <article>
       <h4>
-        <a href={link} target="_blank">{title}</a> &nbsp;
+        <a href={link} target="_blank">{scrubText(title)}</a> &nbsp;
         <small>{display_date}</small>
       </h4>
-      <p>{description}</p>
+      <p>{scrubText(description)}</p>
     </article>
   );
 };
